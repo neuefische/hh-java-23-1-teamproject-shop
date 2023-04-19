3d819a3ff7a8a2005937a8776d0c46fbd4ed53b9
LisaBagszis <lisa.bagszis@gmail.com>
2023-04-19 17:08:17 +0200

    Order option finished

--- frontend/src/static/NavigationBar.tsx
+++ frontend/src/static/NavigationBar.tsx
@@ -5,7 +5,6 @@ import "./NavigationBar.css"
 export default function NavigationBar() {
     return (
         <div className={"NavigationBar"}>
-            <NavLink className={"NavigationItem"} to={"/home"}>Home</NavLink>
             <NavLink className={"NavigationItem"} to={"/menu"}>Menü</NavLink>
             <NavLink className={"NavigationItem"} to={"/order"}>Bestellen</NavLink>
             <NavLink className={"NavigationItem"} to={"/add"}>Hinzufügen</NavLink>
